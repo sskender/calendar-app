525d6e54abc781990bf9c586fe321143276746fd
Sven Skender <sskender@pm.me>
2021-05-03 19:19:14 +0200

    firebase database connection

---
+++ src/services/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_APIKEY,
+  authDomain: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_APPID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// TODO persistance
+const database = firebase.firestore();
+
+export default database;
